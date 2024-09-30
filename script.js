a3b350a253e2f6156bb15f66a8d7c86c65dd31e5
Jeannine Toro <jetg99@gmail.com>
2024-09-29 21:21:35 -0500

    Validacion del campo nombre, email y asunto

--- script.js
+++ script.js
@@ -50,3 +50,80 @@ window.addEventListener('scroll', ()=>{
         });
     }
 });
+
+
+
+
+/*----------  VALIDACIÓN DE FORMULARIO  ----------*/
+
+const nombre = document.getElementById('contacto__nombre');
+const email = document.getElementById('contacto__email');
+const asunto = document.getElementById('contacto__asunto');
+const mensaje = document.getElementById('contacto__mensaje');
+const boton = document.getElementById('contacto__boton');
+const mensajeErrorNombre = document.querySelector('.input__mensaje--nombre');
+const mensajeErrorEmail = document.querySelector('.input__mensaje--email');
+const mensajeErrorAsunto = document.querySelector('.input__mensaje--asunto');
+
+const habilitarBoton = ()=>{
+    
+    let contador = 0;
+
+    if (nombre.value == "") {
+        contador++;
+    }
+    if (email.value == "") {
+        contador++;
+    }
+    if (asunto.value == "") {
+        contador++;
+    }
+    if (mensaje.value == "") {
+        contador++;
+    }
+
+    if (contador == 0) {
+        boton.disabled = false;
+    }else{
+        boton.disabled = true;
+    }
+}
+
+nombre.addEventListener("keyup", habilitarBoton);
+email.addEventListener("keyup", habilitarBoton);
+asunto.addEventListener("keyup", habilitarBoton);
+mensaje.addEventListener("keyup", habilitarBoton);
+
+
+nombre.addEventListener('input', ()=>{
+    if (nombre.value.length > 50) {
+        mensajeErrorNombre.textContent = 'Máximo 50 caracteres';
+        mensajeErrorNombre.classList.add('visible');
+    }else{
+        mensajeErrorNombre.textContent = '';
+        mensajeErrorNombre.classList.remove('visible');
+    }
+});
+
+
+asunto.addEventListener('input', ()=>{
+    if (asunto.value.length > 50) {
+        mensajeErrorAsunto.textContent = 'Máximo 50 caracteres';
+        mensajeErrorAsunto.classList.add('visible');
+    }else{
+        mensajeErrorAsunto.textContent = '';
+        mensajeErrorAsunto.classList.remove('visible');
+    }
+});
+
+email.addEventListener('input', ()=>{
+
+    const correoRegex = /^[a-zA-Z0-9.%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+    if (!correoRegex.test(email.value)) {
+        mensajeErrorEmail.textContent = 'Correo electrónico inválido';
+        mensajeErrorEmail.classList.add('visible');
+    }else{
+        mensajeErrorEmail.textContent = '';
+        mensajeErrorEmail.classList.remove('visible');
+    }
+});
